e5db2da041700ed53ca17e19a8e18dd5b2673e34
katrohas <katrina@discretecat.com>
2024-03-04 20:47:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace bookStoreRecord
+{
+    class Program
+    {
+        public record Bookstore(int ID, string Title, string Author, double Price);
+
+        public static void Main()
+        {
+            Bookstore book1 = new(10, "Stardust", "Neil Gaiman", 19.95);
+            Console.WriteLine($"Object 1: {book1}");
+            Bookstore book2 = new(20, "Good Omens", "Neil Gaiman & Terry Pratchett", 16.50);
+            Console.WriteLine($"Object 2: {book2}");
+            Bookstore book3 = new(30, "American Gods", "Neil Gaiman", 18.40);
+            Console.WriteLine($"Object 3: {book3}");
+
+        }
+    }
+}
